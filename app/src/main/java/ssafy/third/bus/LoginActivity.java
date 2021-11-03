134b5b5e6a87b072aa8fa17efc8e6e44eed29b22
SangjinH <gkstkdwls1994@gmail.com>
2021-11-03 10:27:37 +0900

    feat: 로그인예제 구현

--- app/src/main/java/ssafy/third/bus/LoginActivity.java
+++ app/src/main/java/ssafy/third/bus/LoginActivity.java
@@ -3,8 +3,14 @@ package ssafy.third.bus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText et_id, et_pass;
+    private Button btn_login, btn_register;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
