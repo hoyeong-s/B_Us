a51f38b5b712633acb8bc51f1f20a17c888e2ca5
SangjinH <gkstkdwls1994@gmail.com>
2021-11-14 21:25:48 +0900

    fix: 등록한 버스목록 도착시간 순으로 정렬추가

--- bus/src/main/java/com/ssafy/bus/dto/response/BusResponseDto.java
+++ bus/src/main/java/com/ssafy/bus/dto/response/BusResponseDto.java
@@ -13,20 +13,15 @@ public class BusResponseDto {
     private String busRouteId;
     private String staOrd;
     private String vehId1;
+    private int order;
 
     @Builder
-    public BusResponseDto(String busNo, String remainingTime, String busRouteId, String staOrd, String vehId1) {
+    public BusResponseDto(String busNo, String remainingTime, String busRouteId, String staOrd, String vehId1, int order) {
         this.busNo = busNo;
         this.remainingTime = remainingTime;
         this.busRouteId = busRouteId;
         this.staOrd = staOrd;
         this.vehId1 = vehId1;
+        this.order = order;
     }
-
-//    public static List<BusResponseDto> of(HashMap<String, Object> hashMap) {
-
-//        BusResponseDto dto = BusResponseDto.builder()
-//    }
-
-
 }
