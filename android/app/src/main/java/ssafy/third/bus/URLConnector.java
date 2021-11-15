7a5c2c3bfac16f4f18e58d942f3f90ded49e737f
multicampus <junghoon401@naver.com>
2021-11-15 16:07:08 +0900

    [feat] connect Spring

--- android/app/src/main/java/ssafy/third/bus/URLConnector.java
+++ android/app/src/main/java/ssafy/third/bus/URLConnector.java
@@ -1,63 +1,56 @@
 package ssafy.third.bus;
 
+import android.os.AsyncTask;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
-public class URLConnector extends Thread {
-
-    private String result;
-    private String URL;
-
-    public URLConnector(String url) {
-        URL = url;
-    }
-
+public class URLConnector extends AsyncTask<String, Void, String> {
+    String sendMsg, receiveMsg;
     @Override
-    public void run() {
-        final String output = request(URL);
-        result = output;
-    }
-
-    private String getResult() {
-        return result;
-    }
-
-    private String request(String urlStr) {
-        StringBuilder output = new StringBuilder();
+    // doInBackground의 매개변수 값이 여러개일 경우를 위해 배열로
+    protected String doInBackground(String... strings) {
         try {
-            java.net.URL url = new URL(urlStr);
-            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-            if (conn != null) {
-                conn.setConnectTimeout(10000);
-                conn.setRequestMethod("GET");
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
+            String str = "http://k5a504.p.ssafy.io:8080/api/";
+            str += "busInfo/"+strings[0];
+            URL url = new URL(str);  // 어떤 서버에 요청할지(localhost 안됨.)
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestMethod("GET");                              //데이터를 GET 방식으로 전송합니다.
+            //conn.setDoOutput(true);
+            conn.connect();
 
-                int resCode = conn.getResponseCode();
-                if (resCode == HttpURLConnection.HTTP_OK) {
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream())) ;
-                    String line = null;
-                    while(true) {
-                        line = reader.readLine();
-                        if (line == null) {
-                            break;
-                        }
-                        output.append(line + "\n");
-                    }
+            // 서버에 보낼 값 포함해 요청함.
+//                OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+//                sendMsg = "busInfo/21028"; // GET방식으로 작성해 POST로 보냄 ex) "id=admin&pwd=1234";
+//                osw.write("");                           // OutputStreamWriter에 담아 전송
+//                osw.flush();
 
-                    reader.close();
-                    conn.disconnect();
+            // jsp와 통신이 잘 되고, 서버에서 보낸 값 받음.
+            if(conn.getResponseCode() == conn.HTTP_OK) {
+                InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                BufferedReader reader = new BufferedReader(tmp);
+                StringBuffer buffer = new StringBuffer();
+                while ((str = reader.readLine()) != null) {
+                    buffer.append(str);
                 }
+                receiveMsg = buffer.toString();
+            } else {    // 통신이 실패한 이유를 찍기위한 로그
+                Log.i("통신 결과", conn.getResponseCode()+"에러");
             }
-        } catch(Exception ex) {
-            Log.e("SampleHTTP", "Exception in processing response.", ex);
-            ex.printStackTrace();
-        }
 
-        return output.toString();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 서버에서 보낸 값을 리턴합니다.
+        return receiveMsg;
     }
 }
+
